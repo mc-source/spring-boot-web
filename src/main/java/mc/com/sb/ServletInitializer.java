e312c5255d860bdef6d4bca6074ffbe89132101b
MC <mc69.dashboard@gmail.com>
2020-11-25 10:17:14 +0100

    add banner + ServletInitializer

--- src/main/java/mc/com/sb/ServletInitializer.java
+++ src/main/java/mc/com/sb/ServletInitializer.java
@@ -1,9 +1,11 @@
 package mc.com.sb;
 
-public class ServletInitializer  { //extends SpringBootServletInitializer
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(SpringbootWebApplication.class);
-//	}
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+public class ServletInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootWebApplication.class);
+	}
 }
